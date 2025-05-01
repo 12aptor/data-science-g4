267c04a6b504c53c9bf9272f60bcade64f0ac705
12aptor <paholo2054@gmail.com>
2025-04-30 22:44:52 -0500

    calculadora

--- dia2/01-variables.py
+++ dia2/01-variables.py
@@ -2,6 +2,12 @@ edad = 20
 texto = "Hola mundo!"
 esta_casado = False
 altura = 1.80
+lista = [1, 2, 3, 4, 5]
+diccionario = {
+    "nombre": "Juan",
+    "edad": 20,
+    "esta_casado": False,
+}
 
 mes = input("¿Que mes estamos?: ")
 
