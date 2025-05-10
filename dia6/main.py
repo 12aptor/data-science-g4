7c82c8aca2ff3d4c803fb5bbc6e10aff589b3642
12aptor <paholo2054@gmail.com>
2025-05-09 20:36:06 -0500

    rich y typer

--- dia6/main.py
+++ dia6/main.py
@@ -1,9 +1,24 @@
 import typer
+from rich.panel import Panel
+from rich.console import Console
+from rich.text import Text
 
 app = typer.Typer()
+console = Console()
+
+def show_welcome():
+    """Muestra un mensaje de bienvenida."""
+    print("Hola 🤠!!")
+    console.clear()
+    console.print(
+        Panel.fit(
+            Text("🐍 TEMPORIZADOR PARA PYTHONERS 🐍", justify="center", style="bold bright_cyan"),
+        )
+    )
+    
 
 @app.command()
 def start():
-    print("Hola 😎!!")
+    show_welcome()
 
 app()
