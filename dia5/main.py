74c1617222eaacc8561ce88ba2105c727bc16090
César Mayta Avalos <cesarmayta@gmail.com>
2025-05-21 22:37:41 -0500

    guardamos los usuarios en la base de datos

--- dia5/main.py
+++ dia5/main.py
@@ -5,10 +5,13 @@ if __name__ == "__main__":
     number_of_users = int(input("¿Cuántos usuarios quieres generar? "))
     users = random_user.get_users(number_of_users)
     
-    for user in users:
-        print("="*50)
-        print(f"Nombre: {user['name']['first']} {user['name']['last']}")
-        print(f"Email: {user['email']}")
-        print(f"Ubicación: {user['location']['city']}, {user['location']['country']}")
-        print(f"Fecha de nacimiento: {user['dob']['date']}")
-        print(f"Imagen: {user['picture']['large']}")
+    # for user in users:
+    #     print("="*50)
+    #     print(f"Nombre: {user['name']['first']} {user['name']['last']}")
+    #     print(f"Email: {user['email']}")
+    #     print(f"Ubicación: {user['location']['city']}, {user['location']['country']}")
+    #     print(f"Fecha de nacimiento: {user['dob']['date']}")
+    #     print(f"Imagen: {user['picture']['large']}")
+        
+    random_user.load_users_to_db(users)
+    print(f"se han insertado {number_of_users} usuarios en la base de datos.")
