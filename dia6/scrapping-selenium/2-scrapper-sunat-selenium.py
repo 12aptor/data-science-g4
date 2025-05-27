924a96867e913f4dd81aaf4e33ff28be86bd0f29
César Mayta Avalos <cesarmayta@gmail.com>
2025-05-26 21:38:30 -0500

    scrapper con selenium

---
+++ dia6/scrapping-selenium/2-scrapper-sunat-selenium.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+URL = 'https://www.sunat.gob.pe/'
+
+driver = webdriver.Chrome()
+try:
+    driver.get(URL)
+    time.sleep(3)  # Wait for the page to load
+    precio_venta = driver.find_element(By.ID, 'sell-rate').text
+    print(f'precio venta dolares : {precio_venta}')
+except Exception as e:
+    print(f'Error: {e}')
+finally:
+    driver.quit()
