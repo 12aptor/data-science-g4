e9c482a143cdcf6233efabccbfc55ee74c3a514f
César Mayta Avalos <cesarmayta@gmail.com>
2025-05-26 22:49:42 -0500

    etl basico terminado

--- dia7/1-prefect-basico/1-etl-randomuser.py
+++ dia7/1-prefect-basico/1-etl-randomuser.py
@@ -1,11 +1,12 @@
 from prefect import flow, task
 import requests
 from datetime import datetime
+import mysql.connector
 
 #creamos una tarea
 @task
 def extract():
-    url = "https://randomuser.me/api/?results=1"
+    url = "https://randomuser.me/api/?results=10"
     response = requests.get(url)
     data = response.json()
     return data['results']
@@ -24,11 +25,36 @@ def transform(users):
        
     return transformed_users
 
+@task
+def load(users):
+   conn = mysql.connector.connect(
+       host="localhost",
+       user="root",
+       password="root",
+       database="db_g4"
+   )
+   cursor = conn.cursor()
+   cursor.execute("""
+               CREATE TABLE IF NOT EXISTS random_users (
+               id INT AUTO_INCREMENT PRIMARY KEY,
+               nombre VARCHAR(255),
+               sexo VARCHAR(10),
+               pais VARCHAR(100),
+               fecha_nac DATE)
+               """)
+   conn.commit()
+   insert_query = "INSERT INTO random_users (nombre, sexo, pais, fecha_nac) VALUES (%s, %s, %s, %s)"
+   cursor.executemany(insert_query, users)
+   conn.commit()
+   print(f" {cursor.rowcount} registros insertados en la tabla random_users")
+   cursor.close()
+   conn.close()
+
 @flow(name="ETL Random User")
 def etl_random_user():
     users = extract()
     transformed_users = transform(users)
-    print("Transformed Users:",transformed_users)
+    load(transformed_users)
     
 if __name__ == "__main__":
     etl_random_user()
