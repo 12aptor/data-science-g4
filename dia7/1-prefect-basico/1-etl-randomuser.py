c26b5f158a0cb2209c575bf82bfb1308c2a24700
César Mayta Avalos <cesarmayta@gmail.com>
2025-05-26 22:36:41 -0500

    transform etl

--- dia7/1-prefect-basico/1-etl-randomuser.py
+++ dia7/1-prefect-basico/1-etl-randomuser.py
@@ -10,10 +10,25 @@ def extract():
     data = response.json()
     return data['results']
 
+@task
+def transform(users):
+    # Aquí podrías realizar transformaciones adicionales si es necesario
+    transformed_users = []
+    for user in users:
+       nombre = f"{user['name']['first']} {user['name']['last']}"
+       sexo = user['gender']
+       pais = user['location']['country']
+       fecha_nac = user['dob']['date']
+       fecha_nac = datetime.fromisoformat(fecha_nac.rstrip("Z")).date()
+       transformed_users.append((nombre, sexo, pais, fecha_nac))
+       
+    return transformed_users
+
 @flow(name="ETL Random User")
 def etl_random_user():
     users = extract()
-    print(users)
+    transformed_users = transform(users)
+    print("Transformed Users:",transformed_users)
     
 if __name__ == "__main__":
     etl_random_user()
