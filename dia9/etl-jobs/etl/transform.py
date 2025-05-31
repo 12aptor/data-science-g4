77c8c42fa2ea9da93e97d9afa19cac32661f173d
César Mayta Avalos <cesarmayta@gmail.com>
2025-05-30 21:45:47 -0500

    codigo base

---
+++ dia9/etl-jobs/etl/transform.py
@@ -0,0 +1,5 @@
+from prefect import task
+
+@task
+def transform_jobs():
+    print("TRANSFORMACIÓN DE OFERTAS DE LINKEDIN")
--- dia9/etl-jobs/main.py
+++ dia9/etl-jobs/main.py
@@ -1,10 +1,14 @@
 from prefect import flow
 from etl.extract import extract_jobs
+from etl.transform import transform_jobs
+from etl.load import load_jobs
 
 @flow
 def etl_pipeline():
     print("PIPELINE DE ETL LINKEDIN")
     extract_jobs()
+    transform_jobs()
+    load_jobs()
     print("ETL COMPLETADO")
     
 etl_pipeline()
