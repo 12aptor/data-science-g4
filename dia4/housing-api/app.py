d67c34cea27319e36e7b3c406dea60a3e4455d48
César Mayta Avalos <cesarmayta@gmail.com>
2025-08-08 22:34:03 -0500

    para ml

--- dia4/housing-api/app.py
+++ dia4/housing-api/app.py
@@ -42,6 +42,7 @@ def index():
 @app.route('/housing',methods=['POST'])
 def set_data():
     rooms = request.json['rooms']
+    
     new_housing = Housing(rooms)
     #insertamos el nuevo registro en la base de datos
     db.session.add(new_housing) # insert into housing values(null,rooms)
